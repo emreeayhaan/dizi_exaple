7b6cae4d6af9199a3f36c149eafea56588470b02
Emre <emre-ayhan-00@hotmail.com>
2021-01-03 22:39:47 +0300

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcı dizinin sınırını belirlicek, dizi random dolucak,aynı diziyi tersten doldurcaz.
+            int a = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            int[] dizi = new int[a];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = r.Next(100);
+                Console.WriteLine(dizi[i]);
+            }
+            for (int i = 0; i < dizi.Length / 2; i++)
+            {
+                int b = dizi[dizi.Length - 1 - i];
+                dizi[dizi.Length - 1 - i] = dizi[i];
+                dizi[i] = b;
+            }
+            Console.WriteLine("********************************");
+            foreach (int x in dizi)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
